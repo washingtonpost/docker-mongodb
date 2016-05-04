aac57f4ba9e80f0c6e8eb5d2c40603d35da2dea1
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-04 13:25:13 -0400

    fixed for strict defines in jinja2

--- cloud-compose/templates/system.mounts.sh
+++ cloud-compose/templates/system.mounts.sh
@@ -1,21 +1,21 @@
 # system.mounts.sh
 {%- for volume in aws.volumes %}
 
-{%- if volume.meta and volume.meta.format and not volume.snapshot %}
+{%- if volume.meta is defined and volume.meta.format is defined and volume.snapshot is not defined %}
 mkfs -t {{ volume.file_system }} {{ volume.block }}
 {%- endif %}
 
-{%- if volume.meta and volume.meta.mount %}
+{%- if volume.meta is defined and volume.meta.mount is defined %}
 echo -e '{{ volume.block }}\t{{ volume.meta.mount }}\t{{ volume.file_system }}\t{{ volume.meta.options|default("defaults,noatime", true) }}\t0\t0' >> /etc/fstab
 mkdir -p {{ volume.meta.mount }}
 mount {{ volume.meta.mount }}
 {%- endif %}
 
-{%- if volume.snapshot %}
+{%- if volume.snapshot is defined %}
 resize2fs {{ volume.block }}
 {%- endif %}
 
-{%- if volume.file_system and volume.file_system == "lvm2" %}
+{%- if volume.file_system is defined and volume.file_system == "lvm2" %}
 systemctl enable lvm2-lvmetad.service
 systemctl enable lvm2-lvmetad.socket
 systemctl start lvm2-lvmetad.service
