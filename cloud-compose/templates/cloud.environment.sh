ccfd97533223557696718057648a309e3326384b
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-05 19:51:46 -0400

    updated for awslogs

--- cloud-compose/templates/cloud.environment.sh
+++ cloud-compose/templates/cloud.environment.sh
@@ -1,4 +1,5 @@
 {%- if _node_id is defined %}
-NODE_ID={{ _node_id }}
+NODE_ID="{{ _node_id }}"
 {%- endif %}
+CLUSTER_NAME="{{ name }}"
 INSTANCE_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
