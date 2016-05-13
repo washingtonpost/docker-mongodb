e6144e818aab6f2b36a17d8ab212374a30d4c8d7
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-12 20:45:23 -0400

    added space

--- cloud-compose/templates/mongodb.alias.sh
+++ cloud-compose/templates/mongodb.alias.sh
@@ -6,6 +6,7 @@ baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.2/x86_64/
 gpgcheck=0
 enabled=1
 EOF
+
 yum -y install mongodb-org-shell
 {%- if MONGODB_ADMIN_PASSWORD is defined %}
 cat <<- 'EOF' > /etc/profile.d/mongo.sh
