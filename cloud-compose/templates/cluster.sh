0c30ae32e2c1b3afd92410bbb1ef926540ac7f48
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-21 21:51:34 -0400

    made datadog install if DATADOG_API_KEY is set

--- cloud-compose/templates/cluster.sh
+++ cloud-compose/templates/cluster.sh
@@ -8,6 +8,4 @@
 {% include "docker.config.sh" %}
 {% include "docker_compose.run.sh" %}
 {% include "system.network_conf.sh" %}
-{# Optional template for datadog metrics
 {% include "datadog.mongodb.sh" %}
-#}
--- cloud-compose/templates/datadog.mongodb.sh
+++ cloud-compose/templates/datadog.mongodb.sh
@@ -1,3 +1,4 @@
+{%- if DATADOG_API_KEY is defined %}
 # datadog.mongodb.sh 
 cat <<- 'EOF' > /etc/dd-agent/conf.d/mongo.yaml
 init_config:
@@ -18,3 +19,4 @@ echo '{{node.ip}} node{{node.id}}' >> /etc/hosts
 sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
 echo 'bind_host: 0.0.0.0' >> /etc/dd-agent/datadog.conf
 service datadog-agent restart
+{%- endif %}
