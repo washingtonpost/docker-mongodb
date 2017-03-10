d2a77d27d23cbaecdf1807831cabfc2df5161b94
Patrick Cullen <patrick.cullen@washpost.com>
2017-03-10 07:46:51 -0500

    added backup tools and fixed default mongo version to 3.2

--- cloud-compose/templates/mongodb.alias.sh
+++ cloud-compose/templates/mongodb.alias.sh
@@ -1,12 +1,12 @@
 # mongodb.alias.sh 
 cat <<- EOF > /etc/yum.repos.d/mongodb.repo
-[mongodb-org-${MONGODB_VERSION:-3.4}]
+[mongodb-org-${MONGODB_VERSION:-3.2}]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/${MONGODB_VERSION:-3.4}/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/${MONGODB_VERSION:-3.2}/x86_64/
 gpgcheck=0
 enabled=1
 EOF
-yum -y install mongodb-org-shell
+yum -y install mongodb-org-shell mongodb-org-tools
 {%- if secrets is defined and secrets.MONGODB_ADMIN_PASSWORD is defined %}
 cat <<- EOF > /etc/profile.d/mongo.sh
 alias mongo="mongo -u admin -p ${MONGODB_ADMIN_PASSWORD} --authenticationDatabase admin"
