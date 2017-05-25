1409f23c635f977ca56b24ea3a389e6dd9442a1a
Jason Bartz <jason.bartz@washpost.com>
2017-05-25 11:13:04 -0400

    remove iteritems so it's python 3 compatible

--- cloud-compose/templates/secrets.environment.sh
+++ cloud-compose/templates/secrets.environment.sh
@@ -1,7 +1,7 @@
 # secrets.environment.sh
 {%- if secrets is defined %}
 AWS_REGION="{{ AWS_REGION }}"
-{%- for key, val in secrets.iteritems() %}
+{%- for key, val in secrets.items() %}
 {{ key }}=$(aws kms decrypt --region $AWS_REGION --ciphertext-blob fileb://<(echo '{{ val }}' | base64 -d) --output text | cut -f 2 -d $'\t' | base64 -d)
 {%- endfor %}
 {%- endif %}
